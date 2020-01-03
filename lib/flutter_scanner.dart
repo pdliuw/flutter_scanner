cee2bcebde4c4e0b8cb864ad9fe2f0bd7a94c585
Air <xkrossiapd163@163.com>
2020-01-03 16:19:57 +0800

    Update 'README'

--- lib/flutter_scanner.dart
+++ lib/flutter_scanner.dart
@@ -2,11 +2,9 @@ library flutter_scanner;
 
 ///
 /// Import
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 ///
 /// Part
 part 'src/scanner_widget.dart';
-
